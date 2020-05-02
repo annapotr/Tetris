6563be30e19e6164c253c68605bd3abb2d229eda
Polina Vorobeva <vpd.polina@gmail.com>
2020-05-02 15:17:38 +0300

    replace magic constants

--- Tetris_2.0/fallen.cpp
+++ Tetris_2.0/fallen.cpp
@@ -8,13 +8,13 @@ Fallen::Fallen(Field *f, QGraphicsScene *scene)
 }
 
 QRectF Fallen::boundingRect() const {
-    return QRectF(0,0,250,500);
+    return QRectF(0, 0, BLOCK_PX * field->getFIELD_Ht(), BLOCK_PX * field->getFIELD_W());
 }
 
 void Fallen::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
     for (size_t i = 0; i < field->getFIELD_Ht(); i++) {
         for (size_t j = 0; j < field->getFIELD_W(); j++) {
-            QRectF rec = QRectF(25 * j, 25 * i+10, 25, 25);
+            QRectF rec = QRectF(BLOCK_PX * j, BLOCK_PX * i + 10, BLOCK_PX, BLOCK_PX);
             if (!field->get(i,j).isNull()) {
                 //QPixmap pix(":/red_block.png");
                 QPixmap pix(field->get(i,j));
